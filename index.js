151132923ae9058cd3da4e0ebe2a9522b8ba2dfd
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-05 12:12:39 +0600

    adding server side code for posting

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.vbw8r.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbw8r.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+        const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+
+        app.get('/appointmentOptions',async(req,res)=>{
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
+
+        app.post('/bookings',async(req,res)=>{
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 app.get('/',async(req,res)=>{
     res.send('doctors portal server');
